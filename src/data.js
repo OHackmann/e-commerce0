35b93655a75c80110f730ed2806302f268eaef3d
Hackmann <hackmann657@gmail.com>
2022-06-22 21:44:51 -0300

    adicionando slider

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: 'https://cdn.pixabay.com/photo/2021/10/27/06/05/young-woman-6746060_960_720.png',
+        title: 'SUMMER SALE',
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: 'f5fafd',
+    },
+    {
+        id: 2,
+        img: 'https://cdn.pixabay.com/photo/2021/10/27/06/05/young-woman-6746060_960_720.png',
+        title: 'WINTER SALE',
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: 'fcf1ed',
+    },
+    {
+        id: 3,
+        img: 'https://cdn.pixabay.com/photo/2021/10/27/06/05/young-woman-6746060_960_720.png',
+        title: 'POPULAR SALE',
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: 'f5fafd',
+    }
+]
