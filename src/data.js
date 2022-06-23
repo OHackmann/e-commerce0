683167bf840769188907bd08ffae5a8dbd7c8a5e
Hackmann <hackmann657@gmail.com>
2022-06-23 14:21:17 -0300

    categories

--- src/data.js
+++ src/data.js
@@ -1,23 +1,41 @@
 export const sliderItems = [
     {
         id: 1,
-        img: 'https://cdn.pixabay.com/photo/2021/10/27/06/05/young-woman-6746060_960_720.png',
+        img: 'https://cdn.pixabay.com/photo/2022/03/23/12/36/woman-7087160_960_720.png',
         title: 'SUMMER SALE',
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-        bg: 'f5fafd',
+        bg: 'fcf1ed',
     },
     {
         id: 2,
         img: 'https://cdn.pixabay.com/photo/2021/10/27/06/05/young-woman-6746060_960_720.png',
         title: 'WINTER SALE',
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-        bg: 'fcf1ed',
+        bg: 'f5fafd',
     },
     {
         id: 3,
-        img: 'https://cdn.pixabay.com/photo/2021/10/27/06/05/young-woman-6746060_960_720.png',
+        img: 'https://cdn.pixabay.com/photo/2018/04/06/18/42/model-3296470_960_720.png',
         title: 'POPULAR SALE',
         desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-        bg: 'f5fafd',
+        bg: 'f5f0fd',
     }
-]
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1920&q=80",
+        title: "SHIRT STYLE!",
+    },
+    {
+        id: 2,
+        img: "https://images.unsplash.com/photo-1519306943444-3e1588e3fd23?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        title: "LOUNGWEAR LOVE",
+    },
+    {
+        id: 2,
+        img: "https://images.unsplash.com/photo-1580051215498-3e2b858651b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1025&q=80",
+        title: "LIGHT JACKETS",
+    },
+];
